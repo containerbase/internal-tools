b777f603c612a103498f58743bc81f3c3f43c443
Michael Kriese <michael.kriese@visualon.de>
2020-03-20 18:44:51 +0100

    fix: fix digest regex

--- test/publish-docker.spec.ts
+++ test/publish-docker.spec.ts
@@ -23,7 +23,7 @@ describe(getName(__filename), () => {
       .mockResolvedValueOnce({
         ...res,
         stdout:
-          '[renovate/base@sha256:d694b03ba0df63ac9b27445e76657d4ed62898d721b997372aab150ee84e07a1]',
+          "[renovate/base@sha256:d694b03ba0df63ac9b27445e76657d4ed62898d721b997372aab150ee84e07a1]'\n",
       })
       .mockResolvedValueOnce(res)
       .mockResolvedValueOnce(res)
