af5aca4558011c15d3bcd70ea3c82a46adc1aa94
Michael Kriese <michael.kriese@visualon.de>
2020-04-24 07:16:15 +0200

    fix: logging (#45)

--- src/commands/docker/builder.ts
+++ src/commands/docker/builder.ts
@@ -63,13 +63,14 @@ async function getBuildList({
     )
     .filter((v) => !ignoredVersions.includes(v));
   log(`Found ${allVersions.length} versions within our range`);
+  log(`Candidates:`, allVersions.join(' '));
   latestStable =
     latestVersion ||
     pkgResult.latestVersion ||
     allVersions.filter((v) => ver.isStable(v)).pop();
   log('Latest stable version is ', latestStable);
   const lastVersion = allVersions[allVersions.length - 1];
-  log('Most recent version is ', latestStable);
+  log('Most recent version is ', lastVersion);
   if (lastOnly) {
     log('Building last version only');
     allVersions = [latestStable && !forceUnstable ? latestStable : lastVersion];
@@ -84,7 +85,7 @@ async function getBuildList({
   }
 
   if (buildList.length) {
-    log('Build list: ' + buildList.join(' '));
+    log('Build list: ', buildList.join(' '));
   } else {
     log('Nothing to build');
   }
