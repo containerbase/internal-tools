5e1efe84a7c501bbbf0a587da36dd258493eede9
Michael Kriese <michael.kriese@visualon.de>
2020-04-29 19:22:59 +0200

    chore: add debugging

--- src/utils/datasource/index.ts
+++ src/utils/datasource/index.ts
@@ -1,7 +1,9 @@
 import { getDatasources } from 'renovate/dist/datasource';
 import * as renovate from './renovate-slim';
+import log from '../logger';
 
 export function register(): void {
+  log('register datasources');
   const ds = getDatasources();
   ds.set(renovate.id, Promise.resolve(renovate));
 }
