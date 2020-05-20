700b2d5b85dab45d77625a3ddaaf855360fd01fb
Michael Kriese <michael.kriese@visualon.de>
2020-05-20 14:10:31 +0200

    chore: fix tests

--- test/commands/docker/config.spec.ts
+++ test/commands/docker/config.spec.ts
@@ -2,7 +2,6 @@ import { getName, mocked } from '../../utils';
 import * as _utils from '../../../src/util';
 import { run } from '../../../src/commands/docker/config';
 
-jest.mock('renovate/dist/workers/global/cache');
 jest.mock('renovate/dist/datasource');
 jest.mock('../../../src/util');
 jest.mock('../../../src/utils/docker');
--- test/utils/renovate.spec.ts
+++ test/utils/renovate.spec.ts
@@ -1,6 +1,8 @@
 import * as renovate from '../../src/utils/renovate';
 import { getName } from '../utils';
 
+jest.mock('renovate/dist/util/cache/global/file');
+
 describe(getName(__filename), () => {
   it('works', () => {
     renovate.register();
