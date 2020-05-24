3b79b3a6374ba2f54dd173fc53d339b63ef358fd
Michael Kriese <michael.kriese@visualon.de>
2020-05-24 16:26:44 +0200

    chore: caching

--- test/utils/renovate.spec.ts
+++ test/utils/renovate.spec.ts
@@ -1,8 +1,6 @@
 import * as renovate from '../../src/utils/renovate';
 import { getName } from '../utils';
 
-jest.mock('renovate/dist/util/cache/global/file');
-
 describe(getName(__filename), () => {
   it('works', () => {
     renovate.register();
