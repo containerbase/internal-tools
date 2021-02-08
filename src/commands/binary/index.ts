b806c05f3a7667611237b4476d36bff57c75db3b
Michael Kriese <michael.kriese@visualon.de>
2021-02-08 19:38:13 +0100

    fix(binary-builder): add missing config (#200)
    * fix(binary-builder): add missing config
    
    * fix: linting

--- src/commands/binary/index.ts
+++ src/commands/binary/index.ts
@@ -1,5 +1,5 @@
 import 'source-map-support/register';
-import { getInput, setFailed } from '@actions/core';
+import { setFailed } from '@actions/core';
 import chalk from 'chalk';
 import { ReleaseResult, getPkgReleases } from 'renovate/dist/datasource';
 import { get as getVersioning } from 'renovate/dist/versioning';
@@ -117,7 +117,7 @@ export async function run(): Promise<void> {
     log.info('Builder started');
     const ws = getWorkspace();
 
-    const cfg = await getConfig(getInput('config') || 'builder.json');
+    const cfg = await getConfig();
 
     if (cfg.dryRun) {
       log.warn(chalk.yellow('[DRY_RUN] detected'));
