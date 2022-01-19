c8b21d90285c04a9bdb39a49801ca6cfd045fbeb
Michael Kriese <michael.kriese@visualon.de>
2022-01-19 11:48:40 +0100

    fix: renovate api changes (#271)

--- test/utils/versioning/ubuntu.spec.ts
+++ test/utils/versioning/ubuntu.spec.ts
@@ -3,26 +3,30 @@ import { getName } from '../../utils';
 
 describe(getName(__filename), () => {
   it('isVersion', () => {
-    expect(api.isVersion('bionic')).toBe('bionic');
-    expect(api.isVersion('focal')).toBe('focal');
-    expect(api.isVersion('groovy')).toBeNull();
+    expect(api.isVersion('bionic')).toBe(true);
+    expect(api.isVersion('focal')).toBe(true);
+    expect(api.isVersion('groovy')).toBe(false);
   });
+
   it('isStable', () => {
     expect(api.isStable('bionic')).toBe(true);
-    expect(api.isStable('13.16.3')).toBeUndefined();
-    expect(api.isStable('groovy')).toBeUndefined();
+    expect(api.isStable('16.04')).toBe(true);
+    expect(api.isStable('13.16.3')).toBe(false);
+    expect(api.isStable('groovy')).toBe(false);
   });
 
   it('isCompatible', () => {
     expect(api.isCompatible('bionic', '18.04')).toBe(true);
     expect(api.isCompatible('bionic', '20.04')).toBe(true);
-    expect(api.isCompatible('20.10', '20.04')).toBe(false);
+    expect(api.isCompatible('20.10', '20.04')).toBe(true);
   });
 
   it('sortVersions', () => {
     expect(api.sortVersions('bionic', '18.04')).toBe(0);
     expect(
-      ['18.04', 'bionic', '20.04'].sort((a, b) => api.sortVersions(a, b))
-    ).toEqual(['18.04', 'bionic', '20.04']);
+      ['18.04', 'bionic', '20.04', '16.04'].sort((a, b) =>
+        api.sortVersions(a, b)
+      )
+    ).toEqual(['16.04', '18.04', 'bionic', '20.04']);
   });
 });
