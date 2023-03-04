6f1e50dedd9da7e45661e087b2c760c7ac203ebe
Michael Kriese <michael.kriese@visualon.de>
2023-03-04 13:59:04 +0100

    fix(cosign): suppres input wait

--- src/utils/cosign/common.ts
+++ src/utils/cosign/common.ts
@@ -2,6 +2,5 @@ import { exec } from '../../util';
 import type { ExecResult } from '../types';
 
 export async function cosign(...args: string[]): Promise<ExecResult> {
-  process.env['COSIGN_EXPERIMENTAL'] = 'true';
   return await exec('cosign', [...args]);
 }
