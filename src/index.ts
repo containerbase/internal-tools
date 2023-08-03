c4f87bf77bc1055aeb0ff07d514ff8ffbb653c2d
Michael Kriese <michael.kriese@visualon.de>
2023-08-03 23:27:48 +0200

    fix: ignore re2 errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Node v16 is missing structuredClone, so we polyfill it here.
 import 'core-js/actual/structured-clone';
 import 'source-map-support/register';
+import { env } from 'node:process';
 import run from './runner';
 
+// We don't need it.
+// https://docs.renovatebot.com/self-hosted-experimental/#renovate_x_ignore_re2
+env.RENOVATE_X_IGNORE_RE2 = 'true';
 run().catch(console.error);
