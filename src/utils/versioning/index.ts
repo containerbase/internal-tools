dc9ff5d5bab0cf200aa5879bfe29c7eb1868cd47
Michael Kriese <michael.kriese@visualon.de>
2026-01-28 11:52:42 +0100

    fix!: drop ubuntu versioning override
    - closes #220

--- src/utils/versioning/index.ts
+++ src/utils/versioning/index.ts
@@ -1,9 +1,7 @@
 import { getVersionings } from 'renovate/dist/modules/versioning';
 import log from '../logger';
-import * as ubuntu from './ubuntu';
 
 export function register(): void {
   log('register versionings');
-  const ds = getVersionings();
-  ds.set(ubuntu.id, ubuntu.api);
+  getVersionings();
 }
